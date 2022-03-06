5c0f5cccbe6fb00365aae4fb4810d85b62da64d5
Tailmc <95581499+Tailmc@users.noreply.github.com>
2022-03-06 22:01:45 +0900

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('#mobilemenu')
-const menul = document.querySelector('.navimenu')
-
-menu.addEventListener('click', function(){
-    menu.classList.toggle('active')
-    menul.classList.toggle('activated')
-})
